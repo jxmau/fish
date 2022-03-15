4a3ae11604ea06d9ec75c0c4b81fc3f1a7950ebb
jxmau <jxmau.pro@outlook.fr>
2022-03-15 09:17:17 +0100

    update of the prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,7 +1,4 @@
 function fish_prompt -d "Write out the prompt"
-    # This shows up as USER@HOST /home/user/ >, with the directory colored
-    # $USER and $hostname are set by fish, so you can just use them
-    # instead of using `whoami` and `hostname`
-    printf '%s %s%s%s > ' $USER  \
+    printf ' \U1F916 %s%s%s > '  \
         (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
 end
